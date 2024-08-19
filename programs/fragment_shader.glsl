e73b3b62cf62e2c56027fef483fb3ed4f84aeac3
pavlen5kiy <wolkpavel51@gmail.com>
2024-08-19 19:30:44 +0300

    made the mandelbrot thingy

--- programs/fragment_shader.glsl
+++ programs/fragment_shader.glsl
@@ -5,31 +5,35 @@ out vec4 fragColor;
 uniform vec2 resolution;
 uniform float time;
 
-vec2 rotate2D(vec2 uv, float a) {
-    float s = sin(a);
-    float c = cos(a);
-    return mat2(c, -s, s, c) * uv;
+const float MAX_ITER = 128.0;
+
+float mandelbrot(vec2 uv) {
+    vec2 c = 2.0 * uv - vec2(0.7, 0.0);
+    c = c / pow(time, 4.0) - vec2(0.65, 0.45);
+    vec2 z = vec2(0.0);
+    float iter = 0.0;
+    for (float i; i < MAX_ITER; i++) {
+        z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
+        if (dot(z, z) > 4.0) return iter / MAX_ITER;
+        iter++;
+    }
+    return 0.0;
+}
+
+vec3 hash13(float m) {
+    float x = fract(sin(m) * 5625.246);
+    float y = fract(sin(m + x) * 2216.486);
+    float z = fract(sin(x + y) * 8276.352);
+    return vec3(x, y, z);
 }
 
 void main() {
     vec2 uv = (gl_FragCoord.xy - resolution.xy) / resolution.y;
     vec3 col = vec3(0.0);
 
-    uv = rotate2D(uv, 3.14 / 2.0);
-
-    float r = 0.17;
-    for (float i=0.0; i < 60.0; i++) {
-        float factor = (sin(time) * 0.5 + 0.5) * 0.3;
-        i += factor;
-
-        float a = i / 3;
-        float dx = 2 * r * cos(a) + r * cos(10 * a);
-        float dy = 2 * r * sin(a) + r * sin(10 * a);
-
-        col += 0.001 * factor / length(uv - vec2(dx + 0.1, dy));
-    }
-
-    col *= sin(vec3(0.2, 0.8, 0.9) * time) * 10;
+    float m = mandelbrot(uv);
+    col += hash13(m);
+    col = pow(col, vec3(0.45));
 
     fragColor = vec4(col, 1.0);
 }
